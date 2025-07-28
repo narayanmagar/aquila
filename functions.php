d5cf1aa26dc3082d89e6c91ae189b73b646e15b3
narayanmagar <thmgrnarayan@gmail.com>
2025-07-28 23:47:01 +0200

    function modified

--- functions.php
+++ functions.php
@@ -6,4 +6,11 @@
  */
 
 
+function aquila_enque_scripts(){
+	wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directry() . '/style.css'));
+
+}
+add_action('wp_enque_scripts', 'aquila_enque_scripts');
+
 ?>
+
