f0f35dc72f914958b2ac18c034676953468006fc
narayanmagar <thmgrnarayan@gmail.com>
2025-07-28 19:27:17 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila
+ */
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Wordpress</title>
+</head>
+<body>
+
+</body>
+<h6>Hello Wordpress</h6>
+</html>
