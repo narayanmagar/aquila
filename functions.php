b46f79a17fb89c9877451d300e8411ec3ca98b14
narayanmagar <thmgrnarayan@gmail.com>
2025-07-30 18:11:53 +0200

    enqueue js sheets

--- functions.php
+++ functions.php
@@ -13,7 +13,8 @@
 // functions
 
 function aquila_enqueue_scripts() {
-    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_script('main-js',get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), 'all');
 }
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
 
