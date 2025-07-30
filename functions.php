177fdef5eddcf7b92db6fe63f02f45b374254627
narayanmagar <thmgrnarayan@gmail.com>
2025-07-30 19:11:14 +0200

    add bootstrap and js

--- functions.php
+++ functions.php
@@ -13,8 +13,28 @@
 // functions
 
 function aquila_enqueue_scripts() {
-    wp_enqueue_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_enqueue_script('main-js',get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), 'all');
+
+	//Register Styles
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_register_style('bootstrap-css', get_template_directory_uri() .'/assets/src/library/css/bootstrap.min.css',  [], false, 'all');
+
+
+    //Register Scripts
+    wp_register_script('main-js',get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), 'all');
+    wp_register_script('bootstrap-js', get_template_directory_uri() .'/assets/src/library/css/bootstrap.min.js',  ['jquery'], false, 'all');
+
+
+
+
+    //Enqueue Styles
+    wp_enqueue_style('style-css');
+    wp_enqueue_style('bootstrap-css');
+
+    //Enqueue Scripts
+    wp_enqueue_script('main-js');
+    wp_enqueue_script('bootstrap-js');
+
+
 }
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
 
